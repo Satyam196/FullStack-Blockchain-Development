ac8bf513b186f764e4fcda005c70c84a6250c059
Satyam <satyam.kumar196@gmail.com>
2023-02-06 22:40:16 +0530

    JavaScript Assignment 2 uploaded

---
+++ JavaScript_Assigment/Javascript_Assignment2/PrimeNo.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+let a, b;
+
+a = parseInt(prompt('Enter lower number: '));
+b = parseInt(prompt('Enter higher number: '));
+
+//console.log("Prime numbers between " + a + " and " + b << " are: ");
+
+for (let i = a; i <= b; i++) {
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
